cda47d369ba71f255ddf6e44d9c9c82588230909
Sasha Poplavskiy <saspoplavskiy563@gmail.com>
2023-10-31 10:11:50 +0200

    Finish slider create

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 "use strict";
 window.addEventListener('DOMContentLoaded', ()=>{
     
+    function getSliderImg(nodes){
+        nodes.forEach(function (element, i){
+            element.style.background = `url(../img/slider/Rectangle_${++i}.png) center center/cover no-repeat`;
+        });
+    }
+
+    const parentsSliderBlock = document.querySelector('.slider_block'),
+        sliderBlockForImg = document.querySelectorAll('.slider_content-img');
+
+    getSliderImg(sliderBlockForImg);
 });
